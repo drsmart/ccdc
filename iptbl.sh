74921ac0653209772bb79085e262b6bd3a79b294
Devon Smart <devonsmart90@gmail.com>
2012-05-23 19:18:13 -0700

    shell

--- iptbl.sh
+++ iptbl.sh
@@ -1,1 +1,20 @@
-test.sh
+#ports needed
+#80,443 httpd
+#21 vsftpd
+#3306 mysqld
+
+iptables -F
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+#allow only the following ports
+iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+iptables -A INPUT -p tcp --dport 443 -j ACCEPT
+iptables -A INPUT -p tcp --dport 21 -j ACCEPT
+iptables -A INPUT -p tcp --dport 3306 -j ACCEPT
+iptables -P INPUT DROP
+iptables -P FORWARD DROP
+iptables -P OUTPUT ACCEPT
+iptables -L -v
+
+#saves the new rules
+service iptables save
