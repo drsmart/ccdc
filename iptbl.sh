2a9930fdd42ee45191313fe0d3f99c58ed493355
drsmart <devonsmart90@gmail.com>
2012-05-25 19:06:59 -0700

    Update iptbl.sh

--- iptbl.sh
+++ iptbl.sh
@@ -11,10 +11,10 @@ iptables -A INPUT -i lo -j ACCEPT
 iptables -A INPUT -s 127.0.0.0/8 -j DROP
 iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 #allow only the following ports
-iptables -A INPUT -p tcp --dport 80 -j ACCEPT
-iptables -A INPUT -p tcp --dport 443 -j ACCEPT
-iptables -A INPUT -p tcp --dport 21 -j ACCEPT
-iptables -A INPUT -p tcp --dport 20 -j ACCEPT
+iptables -A INPUT -p tcp --dport 80 -m state --state NEW -j ACCEPT
+iptables -A INPUT -p tcp --dport 443 -m state --state NEW -j ACCEPT
+iptables -A INPUT -p tcp --dport 21 -m state --state NEW -j ACCEPT
+iptables -A INPUT -p tcp --dport 20 -m state --state NEW -j ACCEPT
 iptables -L -v
 
 #saves the new rules
