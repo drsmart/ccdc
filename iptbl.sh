c0f8d4b5ba4499adf72e7475f81d7edac4230805
drsmart <devonsmart90@gmail.com>
2012-05-25 18:39:06 -0700

    Update iptbl.sh

--- iptbl.sh
+++ iptbl.sh
@@ -4,16 +4,17 @@
 #3306 mysqld
 
 iptables -F
+iptables -P INPUT DROP
+iptables -P FORWARD DROP
+iptables -P OUTPUT DROP
 iptables -A INPUT -i lo -j ACCEPT
+iptables -A INPUT -s 127.0.0.0/8 -j DROP
 iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 #allow only the following ports
 iptables -A INPUT -p tcp --dport 80 -j ACCEPT
 iptables -A INPUT -p tcp --dport 443 -j ACCEPT
 iptables -A INPUT -p tcp --dport 21 -j ACCEPT
-iptables -A INPUT -p tcp --dport 3306 -j ACCEPT
-iptables -P INPUT DROP
-iptables -P FORWARD DROP
-iptables -P OUTPUT ACCEPT
+iptables -A INPUT -p tcp --dport 20 -j ACCEPT
 iptables -L -v
 
 #saves the new rules
